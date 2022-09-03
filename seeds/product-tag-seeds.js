80f5277122ee11d2c57e8235e1205f1137e610ce
Brad Teague <b_rad88@live.com>
2022-09-03 19:55:00 +0930

    Make seeding functions async

--- seeds/product-tag-seeds.js
+++ seeds/product-tag-seeds.js
@@ -1,4 +1,4 @@
-const { ProductTag } = require('../models');
+const { ProductTag } = require("../models");
 
 const productTagData = [
   {
@@ -51,6 +51,6 @@ const productTagData = [
   },
 ];
 
-const seedProductTags = () => ProductTag.bulkCreate(productTagData);
+const seedProductTags = async () => await ProductTag.bulkCreate(productTagData);
 
 module.exports = seedProductTags;
