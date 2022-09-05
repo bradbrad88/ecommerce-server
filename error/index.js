128f15e380479bded784040b5e04c64d9c8d2662
Brad Teague <b_rad88@live.com>
2022-09-05 15:22:50 +0930

    Provide foreign key constraint error feedback for users

--- error/index.js
+++ error/index.js
@@ -1,8 +1,17 @@
-const { ValidationError } = require("sequelize");
+const { ValidationError, ForeignKeyConstraintError } = require("sequelize");
 
 const errorHandler = (err, req, res, next) => {
   if (err instanceof ValidationError) {
     return res.json(err);
+  }
+  if (err instanceof ForeignKeyConstraintError) {
+    return res.status(400).json({
+      message:
+        "Foreign key constraint error - a foreign key provided does not exist in the database",
+    });
+  }
+  if (err instanceof SyntaxError) {
+    return res.status(400).json({ message: err.message });
   }
   res.sendStatus(500);
 };
